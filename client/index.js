592e3a18b070e8e5d089f6972d88ddc1b6e57f48
katoztmy <kato.eng.dao@gmail.com>
2025-04-05 14:34:20 +0900

    add comment

--- client/index.js
+++ client/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import { App } from "../src/components/App";
 
+// hydrateRootによってクライアント側でbuttonなどをinteractiveにする、イベントハンドラをつける
 hydrateRoot(document.getElementById("root"), <App />);
