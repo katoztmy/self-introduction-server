64a92226cb746a9cd30d4099307ba74cfef84eee
katoztmy <kato.eng.dao@gmail.com>
2025-04-05 10:53:21 +0900

    add webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,39 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
 module.exports = {
-  entry: "./src/index.js",
+  target: "node",
+  // entry: './server/index.js' - ビルドの開始点となるファイルを指定
+  entry: path.join(__dirname, "server", "index.js"),
+
   output: {
-    path: __dirname + "/dist",
-    filename: "sample.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "server.js",
+  },
+  // nodeExternals() - node_modules内のパッケージをバンドルから除外
+  // サーバーサイドでは必要ないため、バンドルサイズを小さくするために除外します
+  externals: [nodeExternals()],
+
+  // module - ファイルの変換ルールを指定
+  module: {
+    rules: [
+      {
+        // exclude - 変換から除外するファイル
+        // node_modules内のファイルは既に変換されているため除外します
+        exclude: /node_modules/,
+
+        use: {
+          loader: "babel-loader",
+
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
   },
 };
